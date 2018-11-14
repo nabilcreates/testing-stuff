892c6cbc8a9646a23ef57e9895f5b3c6d3d8d3fa
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-15 07:34:20 +0800

    Commenting...

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 
 function StringErrorHandler(e){
 
+    // IT RETURNS SAYING THAT THE STRING (GOT IT FROM THROW NEW STRINGERRORHANDLER) IT NOT A STRING!
     this.toString = function(){
         return `${e} is not a string`
     }
@@ -14,13 +15,26 @@ function StringErrorHandler(e){
 }
 
 function verifyString(str){
+
+    // TRY THIS
     try {
+
+        // IF STR IS A STRING
         if(typeof str === 'string'){
+
+            // CONSOLE.LOG
             console.log('It is a string!')
         }else{
+
+            // THROW THE STR ITSELF
             throw `${str}`
         }
-    } catch (e) {
+    }
+    
+    // IT WILL CATCH ANYTHING THAT IS A THROW
+    catch (e) {
+
+        // AND THROW A NEW ERROR TO THE STRINGERRORHANDLER PASSING THE ERROR AS THE ARG
         throw new StringErrorHandler(e)
     }
 }
